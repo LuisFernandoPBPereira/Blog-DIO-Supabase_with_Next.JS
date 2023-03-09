4d5afc1b0f196f0b8777bfa192bbe66803c034d9
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2023-03-09 00:40:34 -0300

    configuring

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 import { getPosts } from '../utils/mdx-utils';
 
-import Footer from '../../components/Footer';
-import Header from '../../components/Header';
-import Layout, { GradientBackground } from '../../components/Layout';
-import ArrowIcon from '../../components/ArrowIcon';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Layout, { GradientBackground } from '../components/Layout';
+import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
-import SEO from '../../components/SEO';
+import SEO from '../components/SEO';
 
 export default function Index({ posts, globalData }) {
   return (
@@ -28,7 +28,7 @@ export default function Index({ posts, globalData }) {
                 href={`/posts/${post.id}`}
               >
                 <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.created_ate && (
+                  {post.created_at && (
                     <p className="uppercase mb-3 font-bold opacity-60">
                       {post.created_at}
                     </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   mode: 'jit',
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class', // or 'media' or 'class'
-  presets: [require('./src/utils/tailwind-preset')],
+  presets: [require('./utils/tailwind-preset')],
 };
